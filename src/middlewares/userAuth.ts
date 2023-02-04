95d27314f848060297bf26643264aa556a04f41b
Gabriel Gregório <gabriel.gregorio.1@outlook.com>
2023-02-03 23:59:27 -0300

    fix: migrate to custom error

--- src/middlewares/userAuth.ts
+++ src/middlewares/userAuth.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 import { JWT_SECRET } from '@/config/envs';
+import { CustomError } from '@/errors/index';
+import { ErrorEnum } from '@/errors/types';
 import statusCode from '../config/statusCode';
 
 export const isAuthenticate = (authorization) => {
@@ -19,14 +21,14 @@ export const userAuth = (req: Request, res: Response, next: NextFunction) => {
   const authToken = req.headers.authorization;
 
   if (authToken === '' || authToken === undefined) {
-    return res.sendStatus(statusCode.NEED_TOKEN.code);
+    throw new CustomError(ErrorEnum.TOKEN_IS_INVALID_OR_EXPIRED, statusCode.NEED_TOKEN.code);
   }
 
   const bearer = authToken.split(' ');
   const auth = bearer[1];
 
   if (auth === undefined) {
-    return res.sendStatus(statusCode.NEED_TOKEN.code);
+    throw new CustomError(ErrorEnum.TOKEN_IS_INVALID_OR_EXPIRED, statusCode.NEED_TOKEN.code);
   }
 
   try {
@@ -35,14 +37,13 @@ export const userAuth = (req: Request, res: Response, next: NextFunction) => {
     req.data = data;
 
     if (data.username === undefined) {
-      return res.sendStatus(statusCode.NEED_TOKEN.code);
+      throw new CustomError(ErrorEnum.TOKEN_IS_INVALID_OR_EXPIRED, statusCode.NEED_TOKEN.code);
     }
     return next();
   } catch (error) {
     if (error.message === 'jwt expired') {
-      res.statusCode = statusCode.NEED_TOKEN.code;
-      return res.json({ msg: 'jwt expired' });
+      throw new CustomError(ErrorEnum.TOKEN_IS_INVALID_OR_EXPIRED, statusCode.NEED_TOKEN.code);
     }
-    return res.sendStatus(statusCode.NEED_TOKEN.code);
+    throw new CustomError(ErrorEnum.TOKEN_IS_INVALID_OR_EXPIRED, statusCode.NEED_TOKEN.code);
   }
 };
